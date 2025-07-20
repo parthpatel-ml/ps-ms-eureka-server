9b3a2d139c343331b2b6c5e0712e70e174d651b9
parthpatel-ml <parthpatel.ml@gmail.com>
2025-07-20 15:57:06 +0530

    initial commit

---
+++ src/test/java/com/propyassistance/pa_ms_eureka_server/PaMsEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.propyassistance.pa_ms_eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaMsEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
